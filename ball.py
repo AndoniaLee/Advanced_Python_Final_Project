851fc3620b3ce626ab16cc5bf8f20d242be40c9c
AndoniaLee <xl2149@nyu.edu>
2022-07-04 21:46:24 +0200

    Updated the comment of the code

--- ball.py
+++ ball.py
@@ -1,10 +1,9 @@
 import pygame
 
-NORMAL = 0
-SIZE_CHANGING = 1
-SPEED_CHANGING = 2
-
 class Ball:
+    '''
+        Create generic ball class
+    '''
     def __init__(self, img, x, y, d, x_speed, y_speed):
         self.img = img
         self.x = x
@@ -20,6 +19,9 @@ class Ball:
         self.loss_sound = pygame.mixer.Sound("sounds/lifeLoss.wav")
 
     def move(self, screen_width, screen_height):
+        '''
+            Make the ball move along time
+        '''
         if self.y <= 0:
             self.y_speed *= -1
         if self.x >= screen_width - self.d or self.x <= 0:
@@ -31,34 +33,65 @@ class Ball:
         return self.lose, self.score
 
     def draw(self, screen):
+        '''
+            Draw ball image on the screen
+        '''
         if not self.lose:
             screen.blit(self.img, (self.x, self.y))
 
     def update(self):
+        '''
+            The update class is for other ball type. To avoid errors, we put
+            a dummy update class here.
+        '''
         pass
 
     def get_pos(self):
+        '''
+            Return current ball x,y position
+        '''
         return [self.x, self.y]
 
     def get_size(self):
+        '''
+            Return ball size
+        '''
         return self.d
 
     def get_type(self):
-        return NORMAL
+        '''
+            Return ball type
+        '''
+        return "normal"
 
     def set_lose(self):
+        '''
+            Set ball's status to lose, which will lead to game's ending
+        '''
         self.lose = True
 
     def add_score(self):
+        '''
+            Add ball's score by 1
+        '''
         self.score += 1
         
     def reverse_y_speed(self):
+        '''
+            Change the y speed of ball to the opposite
+        '''
         self.y_speed *= -1
 
     def play_lose_sound(self):
+        '''
+            Play loss sound
+        '''
         self.loss_sound.play()
 
     def play_hit_sound(self):
+        '''
+            Play hit sound
+        '''
         self.hit_sound.play()
 
 class Size_Change_Ball(Ball):
@@ -70,9 +103,15 @@ class Size_Change_Ball(Ball):
         self.orig_img = img
     
     def get_type(self):
-        return SIZE_CHANGING
+        '''
+            Return ball type
+        '''
+        return "size changing"
 
     def update(self):
+        '''
+            Update ball's size up to 40, and then shrink it back to normal size
+        '''
         if self.grow > 40:
             self.mode = -1
         if self.grow < 1:
@@ -82,12 +121,6 @@ class Size_Change_Ball(Ball):
         self.d = self.orig_img.get_size()[0] + round(self.grow)
         self.img = pygame.transform.scale(self.orig_img, (self.d, self.d))
 
-    def get_size(self):
-        return self.d
-
-    def draw(self, screen):
-        if not self.lose:
-            screen.blit(self.img, (self.x, self.y))
 
 class Speed_Change_Ball(Ball):
     
@@ -95,12 +128,17 @@ class Speed_Change_Ball(Ball):
         super().__init__(img, x, y, d, x_speed, y_speed)
         self.grow = 0
         self.mode = 1
-        self.orig_img = img
     
     def get_type(self):
-        return SPEED_CHANGING
+        '''
+            Return ball type
+        '''
+        return "speed changing"
 
     def update(self):
+        '''
+            Update ball's speed up to 1, and then shrink it back to normal speed
+        '''
         if self.grow > 1:
             self.mode = -1
         if self.grow < 1:
@@ -110,9 +148,3 @@ class Speed_Change_Ball(Ball):
         self.x_speed += self.grow
         self.y_speed += self.grow
 
-    def get_size(self):
-        return self.d
-
-    def draw(self, screen):
-        if not self.lose:
-            screen.blit(self.img, (self.x, self.y))
--- pong_oop.py
+++ pong_oop.py
@@ -1,30 +1,54 @@
+'''
+Created By  : Xiaonan Li / Peiyu Xiao / Xinyu Wang / Xujia Li
+Created Date: 04 / 07 / 2022
+Created For : Advanced Programming of Python Final Project
+'''
+
 import pygame
 from ball import *
 import random
 
-# initialize the game
+
+'''
+Game Initialization:
+    1) Set up the macros, constants and flags
+    2) Load the images, sound effects and fonts
+'''
+
+print("---------------------------------------------------------------------------")
+print("Game Initializing...")
+
+# macros
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
-pygame.init()
-screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
-pygame.display.set_caption("Pong!")
-
-# load images
-# background
-bg = pygame.image.load("images/bg.jpg")
-
-# some constants
 BEGINNING = 0
 PLAYING = 1
 END = 2
 WHITE = (255, 255, 255)
 
-# fonts
+# initialize pygame
+pygame.init()
+screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+pygame.display.set_caption("Pong!")
+
+# load images and fonts
+bg = pygame.image.load("images/bg.jpg")
 title_font = pygame.font.SysFont("Arial Black",60)
 font = pygame.font.SysFont("Ayuthaya",24)
 
+
+
+
+'''
+Functions:
+    Set up functions that is used for the game.
+'''
+
 def check_collide(ball, p_x, p_y, p_w):
+    '''
+        Parameters: ball instance, paddle's x and y coordinates and paddle's width
+        Return: None. But change the ball's status based on calculations
+    '''
     lose = ball.lose
     if lose:
         return
@@ -35,12 +59,21 @@ def check_collide(ball, p_x, p_y, p_w):
         if b_x + (b_d / 2) * 0.7 > p_x + p_w \
             or b_x + (b_d / 2) * 1.3 < p_x:
             ball.set_lose()
+            ball.play_lose_sound()
         else: # collide
+            ball.play_hit_sound()
             ball.add_score()
             ball.reverse_y_speed()
-
+'''
+Main:
+    Main function that contains the game loop.
+'''
 
 def main():
+
+    #### Variable Initialization ####
+
+    # initialize score and status
     score = 0
     status = BEGINNING
    
@@ -62,16 +95,29 @@ def main():
     ball_list = []
     ball_list.append(Ball(ball_img, ball_x, ball_y, ball_d, ball_x_speed, ball_y_speed))
 
+    # initialize timer
     timer = 0
     timer_update = 0
     if_update = False
     
-    # main loop
-    while 1:
+    #### Main While Loop ####
+
+    print("---------------------------------------------------------------------------")
+    print("Game at beginning stage")
+    
+    while True:
+
+        # Event monitoring
         
         for event in pygame.event.get():
+
+            # Monitor quit button
             if event.type == pygame.QUIT:
+                print("---------------------------------------------------------------------------")
+                print("Quiting game...")
                 return
+
+            # Monitor keyboard inputs
             elif event.type == pygame.KEYDOWN:
                 key = event.key
                 if key == pygame.K_RIGHT:
@@ -82,23 +128,33 @@ def main():
                     status = PLAYING
                     timer = 0
                     timer_update = 0
+                    print("---------------------------------------------------------------------------")
+                    print("Game at playing stage")  
                 elif key == pygame.K_r and status == END:
                     status = BEGINNING
+                    print("---------------------------------------------------------------------------")
+                    print("Game restarting...")
             elif event.type == pygame.KEYUP:
                 paddle_speed = 0
 
+        # Status checking
         if status == PLAYING:
+            
             screen.blit(bg, (0, 0))
             timer += 1
             timer_update += 1
+
+            # Generate a random ball at random place every 1000 frames
             if timer == 1000:
                 ball_x = random.randint(100,SCREEN_WIDTH)
                 ball_y = random.randint(40,SCREEN_HEIGHT/3)
                 which_ball = random.choice([Ball,Speed_Change_Ball,Size_Change_Ball])
-                ball_list.append(which_ball(ball_img, ball_x, ball_y, ball_d, ball_x_speed, ball_y_speed))
+                new_ball = which_ball(ball_img, ball_x, ball_y, ball_d, ball_x_speed, ball_y_speed)
+                print("A",new_ball.get_type(),"ball is generated...")
+                ball_list.append(new_ball)
                 timer = 0
             
-            # ball position change
+            # Change ball position, update ball's speed or size based on its class
             score = 0
             if timer_update == 20:
                 if_update = True
@@ -108,6 +164,8 @@ def main():
                 [lose, ball_score] = ball.move(SCREEN_WIDTH, SCREEN_HEIGHT)
                 score += ball_score
                 if lose:
+                    print("---------------------------------------------------------------------------")
+                    print("Game at ending stage")
                     status = END
                     break
                 
@@ -116,33 +174,32 @@ def main():
                 if if_update:
                     ball.update()
                 ball.draw(screen)
+
             if if_update:
                 if_update = False
             
-            # paddle position change
+            # Change paddle position 
             paddle_x += paddle_speed
             if paddle_x + paddle_width >= SCREEN_WIDTH:
                 paddle_x = SCREEN_WIDTH - paddle_width
             if paddle_x <= 0:
                 paddle_x = 0
-
-            # draw
-
             screen.blit(paddle, (paddle_x, paddle_y))
 
-            # show score
+            # Show score
             gamestring = " Score: "+str(score)
             text = font.render(gamestring,True,WHITE)
             screen.blit(text,(50,50))
 
         elif status == BEGINNING:
-
-            screen.blit(bg, (0, 0))
             
+            screen.blit(bg, (0, 0))
+
+            # Add the first ball
             ball_list = []
             ball_list.append(Ball(ball_img, ball_x, ball_y, ball_d, ball_x_speed, ball_y_speed))
 
-            # show text
+            # Show text
             welcomestring = "MINI PONG"
             text = title_font.render(welcomestring,True,WHITE)
             screen.blit(text,(220,200))
@@ -156,8 +213,11 @@ def main():
             screen.blit(text,(250,400))
 
         elif status == END:
+
+
             screen.blit(bg, (0, 0))
-            
+
+            # Show text
             welcomestring = "GAME OVER"
             text = title_font.render(welcomestring,True,WHITE)
             screen.blit(text,(220,200))
@@ -174,3 +234,6 @@ def main():
 
 main()
 pygame.quit()
+print("---------------------------------------------------------------------------")
+print("Game quitted...")
+print("---------------------------------------------------------------------------")
